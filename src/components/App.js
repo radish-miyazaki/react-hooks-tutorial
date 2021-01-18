1248f0a3da41529d6c874bd07e3a9c9904d98de1
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2021-01-18 21:14:59 +0900

    implement create event & delete all events

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,60 @@
-import React from 'react'
+import React, { useState, useReducer } from 'react'
+import reducer from '../reducers'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addEvent = e => {
+    e.preventDefault()
+
+    dispatch({ type: 'CREATE_EVENT', title, body })
+
+    setTitle('')
+    setBody('')
+  }
+
+  const deleteAllEvents = e => {
+    e.preventDefault()
+
+    dispatch({ type: 'DELETE_ALL_EVENTS' })
+  }
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input type="text" className="form-control" id="formEventTitle" />
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            type="text"
+            className="form-control"
+            id="formEventTitle" />
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">内容</label>
-          <textarea className="form-control" id="formEventBody" />
+          <textarea
+            value={body}
+            onChange={e => setBody(e.target.value)}
+            className="form-control"
+            id="formEventBody" />
         </div>
-        <button className="btn btn-primary">イベントを作成</button>
-        <button className="btn btn-danger">全てのイベントを削除</button>
+        <button
+          className="btn btn-primary"
+          onClick={addEvent}
+        >
+          イベントを作成
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllEvents}
+        >
+          全てのイベントを削除
+        </button>
       </form>
 
       <h4>イベント一覧</h4>
